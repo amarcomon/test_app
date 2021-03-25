0c398b388994a362d6cff0fe7475ab210749b953
amarcomon <a.marcomon@gmail.com>
2021-03-26 00:50:35 +0100

    feat(form): add control errors

--- src/app/components/user-validation/user-validation.component.ts
+++ src/app/components/user-validation/user-validation.component.ts
@@ -7,16 +7,43 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./user-validation.component.scss'],
 })
 export class UserValidationComponent implements OnInit {
-
   public form: FormGroup;
-  private static readonly MAIL_PATTERN = '[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}'
+  public errorMail: boolean;
+  public errorPassword: boolean;
+  private static readonly MAIL_PATTERN =
+    '[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    const mail = new FormControl('', [Validators.required, Validators.pattern(UserValidationComponent.MAIL_PATTERN)])
-    const password = new FormControl('', [Validators.required, Validators.minLength(5)])
-    this.form = new FormGroup({mail, password})
+    const mail = new FormControl('', [
+      Validators.required,
+      Validators.pattern(UserValidationComponent.MAIL_PATTERN),
+    ]);
+    const password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]);
+    const remind = new FormControl(false);
+    this.form = new FormGroup({ mail, password, remind });
   }
 
+  submit() {
+    this.checkFormValidation();
+    console.log('Form submitted', this.form.value);
+  }
+
+  clearErrors() {
+    this.errorMail = false;
+    this.errorPassword = false;
+  }
+
+  private checkFormValidation() {
+    if (this.form.get('mail').errors) {
+      this.errorMail = !!this.form.get('mail').errors;
+    }
+    if (this.form.get('password').errors) {
+      this.errorPassword = !!this.form.get('password').errors;
+    }
+  }
 }
